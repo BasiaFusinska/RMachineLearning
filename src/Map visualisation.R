e7a60af7a527fc6223ab26bf50e53f90e55e3321
Barbara Fusinska <barbara.fusinska@gmail.com>
2017-06-13 21:49:43 +0100

    refactoring of the Map visualisation code

--- src/Map visualisation.R
+++ src/Map visualisation.R
@@ -17,6 +17,7 @@ levels(crimeData$Primary.Type)
 
 # loading the required packages
 library(ggplot2)
+install.packages('ggmap')
 library(ggmap)
 
 # Get map from Google
@@ -35,44 +36,23 @@ ggmap(map_g) +
   geom_point(data = crimeData, aes(x = Longitude, y = Latitude, fill = Primary.Type, alpha = 0.8), size = 1, shape = 21) +
   guides(fill=FALSE, alpha=FALSE, size=FALSE)
 
-# Select only Assault, Burglary, Crim Sexual Assult
+# Select only Assault, Burglary types
 crimeTypes <- levels(crimeData$Primary.Type)
 crimeTypes
-selectedCrimes <- subset(crimeData, Primary.Type %in% c(crimeTypes[2], crimeTypes[4], crimeTypes[6]))
+selectedCrimes <- subset(crimeData, Primary.Type %in% c(crimeTypes[2], crimeTypes[4]))
 
 ggmap(map_g) +
   geom_point(data = selectedCrimes, aes(x = Longitude, y = Latitude, fill = Primary.Type, alpha = 0.8), size = 1, shape = 21) +
   guides(fill=FALSE, alpha=FALSE, size=FALSE)
 
-# Clustering
-dataColumns <- selectedCrimes[, c('Longitude', 'Latitude')]
-originalData <- selectedCrimes
-clusterSize <- 6
+# Clustering - kmeans (k = 6)
+clusterResult <- kmeans(selectedCrimes[, c('Longitude', 'Latitude')], 6)
 
-clusterResult <- kmeans(dataColumns, clusterSize, algorithm = c("Hartigan-Wong", "Lloyd", "Forgy", "MacQueen"))
+# Get cluster information
 centers <- as.data.frame(clusterResult$centers)
 clusterColours <- factor(clusterResult$cluster)
+
 ggmap(map_g) +
-  geom_point(data = originalData, aes(x = Longitude, y = Latitude, alpha = 0.8, color = clusterColours), size = 1) +
+  geom_point(data = selectedCrimes, aes(x = Longitude, y = Latitude, alpha = 0.8, color = clusterColours), size = 1) +
   geom_point(data = centers, aes(x = Longitude, y = Latitude, alpha = 0.8), size = 1.5) +
-  guides(fill=FALSE, alpha=FALSE, size=FALSE)
-
-clusterSize <- 10
-clusterResult <- kmeans(dataColumns, clusterSize, algorithm = c("Hartigan-Wong", "Lloyd", "Forgy", "MacQueen"))
-centers <- as.data.frame(clusterResult$centers)
-clusterColours <- factor(clusterResult$cluster)
-ggmap(map_g) +
-  geom_point(data = originalData, aes(x = Longitude, y = Latitude, alpha = 0.8, color = clusterColours), size = 1) +
-  geom_point(data = centers, aes(x = Longitude, y = Latitude, alpha = 0.8), size = 1.5) +
-  guides(fill=FALSE, alpha=FALSE, size=FALSE)
-
-
-# Clusters using Primary.Type 
-selectedCrimes$Primary.Type <- as.double(selectedCrimes$Primary.Type)
-
-dataColumns <- selectedCrimes[, c('Longitude', 'Latitude', 'Primary.Type')]
-clusterResult <- kmeans(dataColumns, clusterSize, algorithm = c("Hartigan-Wong", "Lloyd", "Forgy", "MacQueen"))
-clusterColours <- factor(clusterResult$cluster)
-ggmap(map_g) +
-  geom_point(data = originalData, aes(x = Longitude, y = Latitude, alpha = 0.8, color = clusterColours), size = 1) +
   guides(fill=FALSE, alpha=FALSE, size=FALSE)
